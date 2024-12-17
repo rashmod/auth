136fade8b26e65aca3b9a288b5f0e6ed85006b20
Bhushan <alhatbhushan@gmail.com>
2024-12-17 22:49:43 +0530

    [backend] feat: setup basic express middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,5 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
 
 import env from '@/config/env';
@@ -6,6 +8,10 @@ import connectDB from '@/db/prisma';
 const app = express();
 
 connectDB();
+
+app.use(express.json());
+app.use(cors({ origin: env.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
 
 app.listen(env.PORT, () => {
 	console.log(`Server is running on port ${env.PORT} in ${env.NODE_ENV} mode`);
