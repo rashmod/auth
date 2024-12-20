4d6620873465a3a25eaad928428c146dcdda3529
Bhushan <alhatbhushan@gmail.com>
2024-12-20 15:12:08 +0530

    [frontend] fix: used import in tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import animate from 'tailwindcss-animate';
+
 export default {
 	darkMode: ['class'],
 	content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
@@ -60,5 +62,5 @@ export default {
 			},
 		},
 	},
-	plugins: [require('tailwindcss-animate')],
+	plugins: [animate],
 };
